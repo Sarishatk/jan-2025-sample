7fe53d3ad83872ca6769b9b7767719b2e09fd83d
Sarisha.tk <sarishakrishna58@gmail.com>
2026-01-15 19:59:09 +0530

    c6

--- userapi/models.py
+++ userapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Prductmodel(models.Model):
+
+    product_name = models.CharField(max_length=20)
+
+    product_clr = models.CharField(max_length=20)
+
+    prduct_price = models.IntegerField()
